0a0170ad093b81c4b99f8a99e4f571dafcf23e9c
Robert Kemp <robert.kemp04@outlook.com>
2023-10-26 17:14:10 -0500

    Starter code

---
+++ MusicLibraryWebAPI/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MusicLibraryWebAPI.Data
+{
+    public class ApplicationDBContext : DbContext    
+    {
+        public ApplicationDBContext(DbContextOptions
+            options) : base(options)
+        { 
+
+        }
+    }
+}
---
+++ MusicLibraryWebAPI/Models/Music.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Identity.Client;
+
+namespace MusicLibraryWebAPI.Models
+{
+    public class Music
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string genre { get; set; }
+
+    }
+}
